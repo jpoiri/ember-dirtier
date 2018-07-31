7ea73f6bf2f0b5fdf0afb64a65ed579b63ad139b
jpoiri <justin.poirier@gmail.com>
2018-07-31 10:49:53 -0300

    Update sandbox.js

--- tests/dummy/app/routes/sandbox.js
+++ tests/dummy/app/routes/sandbox.js
@@ -1,15 +1,6 @@
 import Route from '@ember/routing/route';
 import DS from 'ember-data';
 
-const { Model } = DS;
-
 export default Route.extend({
-    model() {
 
-        //console.dir(Model);
-
-        //return this.get('store').createRecord('artist');
-
-        //return this.get('store').findAll('artist');
-    },
 });
